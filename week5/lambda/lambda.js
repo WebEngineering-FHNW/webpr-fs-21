b73217d2b9145536bc4c13170474a036090204d7
Dierk Koenig <dierk.koenig@canoo.com>
2021-03-24 09:03:17 +0100

    y-combinator

--- week5/lambda/lambda.js
+++ week5/lambda/lambda.js
@@ -3,6 +3,15 @@
 const id    = x =>      x;
 const konst = x => y => x;
 
+// id(id) === id
+// const M = f => f(f);       // Mockingbird
+// M(M)                       // replace by definition
+// (f => f(f)) (f => f(f))    // alpha translation
+// (f => f(f)) (g => g(g))    // beta reduction, apply function
+// ((g => g(g))((g => g(g)))) // collapse parentheses
+// (g => g(g)) (g => g(g))    // alpha translate
+// (f => f(f)) (f => f(f))    // reproduction reached
+// const rec = M(M);          // core of y-combinator
 
 // derived
 const F = konst (id);
