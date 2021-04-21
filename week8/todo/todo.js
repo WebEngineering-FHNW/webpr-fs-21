caa168a265431eb81bb79b5da119eea54d9e8f7a
Dierk Koenig <dierk.koenig@canoo.com>
2021-04-21 09:59:18 +0200

    after week 8

--- week8/todo/todo.js
+++ week8/todo/todo.js
@@ -1,8 +1,25 @@
 
+let todoCount = 0;
+
 function startTodo() {
 
 }
 
 function addTodo() {
+    
+    todoCount++;
+
+    const container = document.getElementById("todoContainer");
+
+    container.innerHTML += `    
+    <tr>
+        <td>
+            <label for="todo${todoCount}text">Todo #${todoCount} text</label>
+            <input  id="todo${todoCount}text" value="..." type="text"></td>
+        <td>
+            <label for="todo${todoCount}done">Todo #${todoCount} done</label>
+            <input  id="todo${todoCount}done"  type="checkbox"></td>
+    </tr>
+    `;
     
 }
