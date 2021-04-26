68c0b4c8ba12181374729efe465dbf8f79c8bef5
Dierk Koenig <dierk.koenig@canoo.com>
2021-04-26 09:37:14 +0200

    innerText to textContent

--- week9/Milestone_1/todo/todoTest.js
+++ week9/Milestone_1/todo/todoTest.js
@@ -6,29 +6,29 @@ test("todo-crud", assert => {
     // setup
     todoContainer = document.createElement("div");
     numberOfTasks = document.createElement("span");
-    numberOfTasks.innerText = '0';
+    numberOfTasks.textContent = '0';
     openTasks = document.createElement("span");
-    openTasks.innerText = '0';
+    openTasks.textContent = '0';
 
     startTodo(todoContainer, numberOfTasks, openTasks);
 
     const elementsPerRow = 3;
 
     assert.equals(todoContainer.children.length, 0*elementsPerRow);
-    assert.equals(numberOfTasks.innerText, '0');
-    assert.equals(openTasks.innerText, '0');
+    assert.equals(numberOfTasks.textContent, '0');
+    assert.equals(openTasks.textContent, '0');
 
     addTodo();
 
     assert.equals(todoContainer.children.length, 1*elementsPerRow);
-    assert.equals(numberOfTasks.innerText, '1');
-    assert.equals(openTasks.innerText, '1');
+    assert.equals(numberOfTasks.textContent, '1');
+    assert.equals(openTasks.textContent, '1');
 
     addTodo();
 
     assert.equals(todoContainer.children.length, 2*elementsPerRow);
-    assert.equals(numberOfTasks.innerText, '2');
-    assert.equals(openTasks.innerText, '2');
+    assert.equals(numberOfTasks.textContent, '2');
+    assert.equals(openTasks.textContent, '2');
 
     const firstCheckbox = todoContainer.querySelectorAll("input[type=checkbox]")[0];
     assert.equals(firstCheckbox.checked, false);
@@ -37,8 +37,8 @@ test("todo-crud", assert => {
 
     assert.equals(firstCheckbox.checked, true);
 
-    assert.equals(numberOfTasks.innerText, '2');
-    assert.equals(openTasks.innerText, '1');
+    assert.equals(numberOfTasks.textContent, '2');
+    assert.equals(openTasks.textContent, '1');
 
     // add a test for the deletion of a todo-item
 
@@ -47,15 +47,15 @@ test("todo-crud", assert => {
     const firstDeleteBtn = todoContainer.querySelectorAll("input[type=submit]")[0];
     firstDeleteBtn.click();
 
-    assert.equals(numberOfTasks.innerText, '1');
-    assert.equals(openTasks.innerText, '1');      // remains!
+    assert.equals(numberOfTasks.textContent, '1');
+    assert.equals(openTasks.textContent, '1');      // remains!
 
     // delete an unchecked item
 
     const secondDeleteBtn = todoContainer.querySelectorAll("input[type=submit]")[0];
     secondDeleteBtn.click();
 
-    assert.equals(numberOfTasks.innerText, '0');
-    assert.equals(openTasks.innerText, '0');      // changes
+    assert.equals(numberOfTasks.textContent, '0');
+    assert.equals(openTasks.textContent, '0');      // changes
 
 });
