adff4b552db2ecb3e3c8e66ea1880bbba9c23261
Dierk Koenig <dierk.koenig@canoo.com>
2021-05-26 08:35:25 +0200

    refresher reduce 1

--- week11/util/utilTest.js
+++ week11/util/utilTest.js
@@ -13,3 +13,13 @@ test("util-times", assert => {
     assert.is(collect[9], 9);
 
 });
+
+test("util-refresher", assert => {
+    const ary = [0,1,2,3,4];
+    const sum  = ary.reduce( (acc, current) => acc + current, 0);
+    assert.is(sum, 10);
+    const prod = ary.reduce( (acc, current) => acc * current, 1);
+    assert.is(prod, 0);
+
+
+});
