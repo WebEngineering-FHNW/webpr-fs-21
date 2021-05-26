c41711fb4a17e08f066e05a3f529bebf4d61e6aa
Dierk Koenig <dierk.koenig@canoo.com>
2021-05-26 09:39:42 +0200

    refresher reduce 3

--- week11/util/utilTest.js
+++ week11/util/utilTest.js
@@ -27,8 +27,30 @@ test("util-refresher", assert => {
     }, []);
     assert.is(ary.length , cpy.length);
     assert.is(ary[0]     , cpy[0]);
-    assert.is(ary[-1]    , cpy[-1]);
+    assert.is(ary[ary.length-1]    , cpy[ary.length-1]);
 
     // array.reverse mit reduce
+    const rvs = ary.reduce((acc, current) => (acc.unshift(current), acc), []);
+    assert.is(ary.length            , rvs.length);
+    assert.is(ary[0]                , rvs[ary.length-1]);
+    assert.is(ary[ary.length-1]     , rvs[0]);
+
+    // variante mit rest operator
+    const rvs2 = ary.reduce((acc, current) => [current, ...acc], []);
+    assert.is(ary.length        , rvs2.length);
+    assert.is(ary[0]            , rvs2[ary.length-1]);
+    assert.is(ary[ary.length-1] , rvs2[0]);
+
+    const mixedBools = [true, false, false, true, false];
+    const trueBools  = [true, true, true];
+    const falseBools = [false, false, false, false];
+
+    const hasTrue = bools => bools.reduce((acc , current) => acc || current, false);
+    assert.is(hasTrue([]), false);
+    assert.is(hasTrue([true]), true);
+    assert.is(hasTrue([false]), false);
+    assert.is(hasTrue(mixedBools), true);
+    assert.is(hasTrue(trueBools),  true);
+    assert.is(hasTrue(falseBools), false);
 
 });
